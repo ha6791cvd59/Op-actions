7996cd3f817877d18414bbf89073c0bc091ef08a
ha6791cvd59 <54299456+ha6791cvd59@users.noreply.github.com>
2020-10-02 09:29:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,30 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-#git clone https://github.com/kenzok8/openwrt-packages.git  package/openwrt-packages
-#git clone https://github.com/kenzok8/small.git  package/openwrt-small
-
-git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/luci-app-koolproxyR
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-git clone https://github.com/honwen/luci-app-aliddns.git package/luci-app-aliddns
-#git clone https://github.com/Apocalypsor/luci-app-smartdns.git package/luci-app-smartdns
-git clone -b master https://github.com/vernesong/OpenClash package/OpenClash
-git clone https://github.com/xiaorouji/openwrt-package.git package/Lienol
-#git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
-#git clone https://github.com/fw876/helloworld.git package/helloworld
-svn co https://github.com/pkpkgtr1/pkpkgtr-openwrt-package/trunk/luci-app-advancedsetting package/luci-app-advancedsetting
-#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
-git clone https://github.com/fw876/helloworld.git package/helloworld
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/jerrykuku
-
-#================================================= 主题
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-opentomcat package/luci-theme-opentomcat
-#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial package/luci-theme-atmaterial
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
-
-
-sed -i 's/1.14/1.15/g' feeds/packages/lang/golang/golang/Makefile
-sed -i 's/=4/=2/g' feeds/packages/lang/golang/golang/Makefile
-sed -i 's/7011af3bbc2ac108d1b82ea8abb87b2e63f78844f0259be20cde4d42c5c40584/28bf9d0bcde251011caae230a4a05d917b172ea203f2a62f2c2f9533589d4b4d/g' feeds/packages/lang/golang/golang/Makefile
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/kenzok8/openwrt-packages.git  package/openwrt-packages
+git clone https://github.com/kenzok8/small.git  package/openwrt-small
